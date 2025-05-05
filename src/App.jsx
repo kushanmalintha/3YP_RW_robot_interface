a9d4de8142d4dcaeafe5dce857193cd1331fb4b5
kushan malintha <e20243@eng.pdn.ac.lk>
2025-05-06 00:38:20 +0530

    remove files

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import MenuPage from "./pages/MenuPage";
-import OrderDetailsPage from "./pages/OrderDetailsPage";
 
-const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<MenuPage />} />
-      <Route path="/order/:itemId" element={<OrderDetailsPage />} />
-    </Routes>
-  );
-};
+const App = () => {};
 
 export default App;
