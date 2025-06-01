e350f08b8727076abd8dc93f9d3646f502c2429f
RaviAlpha <ravindupathirage12345@gmail.com>
2025-06-01 17:11:21 +0530

    Topic sharing is done

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://192.168.8.199:3000';
-const WS_BASE_URL = 'ws://192.168.8.199:3000'; // for WebSocket connections
+const API_BASE_URL = 'http://3.27.157.127:3000';
+const WS_BASE_URL = 'ws://3.27.157.127:3000'; // for WebSocket connections
 
 export { API_BASE_URL, WS_BASE_URL };
