61f9f92b89781bf22ba69116700ba0cded69724a
kushan malintha <e20243@eng.pdn.ac.lk>
2025-07-03 22:30:08 +0530

    make ws to wss

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 // const API_BASE_URL = 'http://3.27.157.127:3000';
-const WS_BASE_URL = 'wss://3.27.157.127:3000'; // for WebSocket connections
+// const WS_BASE_URL = 'ws://3.27.157.127:3000'; // for WebSocket connections
+const WS_BASE_URL = 'wss://restaurant-server.duckdns.org'; // for WebSocket connections
 // const API_BASE_URL = 'http://localhost:3000';
 const API_BASE_URL = 'https://restaurant-server.duckdns.org';
 
