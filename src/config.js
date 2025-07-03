2b6f42e92db8d3c712dbbbad3b9f9fe8b3f3d1fd
kushan malintha <e20243@eng.pdn.ac.lk>
2025-07-03 18:36:55 +0530

    make https request

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
-const API_BASE_URL = 'http://3.27.157.127:3000';
+// const API_BASE_URL = 'http://3.27.157.127:3000';
 const WS_BASE_URL = 'ws://3.27.157.127:3000'; // for WebSocket connections
 // const API_BASE_URL = 'http://localhost:3000';
+const API_BASE_URL = 'https://restaurant-server.duckdns.org/api';
 
 export { API_BASE_URL, WS_BASE_URL };
