c3533f65da2a0f8a70e8dc9126614619bd9ddce1
kushan malintha <e20243@eng.pdn.ac.lk>
2025-09-17 11:56:22 +0530

    fix

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 //const API_BASE_URL = 'http://3.27.157.127:3000';
-// const WS_BASE_URL = 'ws://localhost:3000'; // for WebSocket connections
+const WS_BASE_URL = 'ws://192.168.8.148:3000'; // for WebSocket connections
 // const WS_BASE_URL = 'ws://3.27.157.127:3000'; // for WebSocket connections
-const WS_BASE_URL = 'wss://restaurant-server.duckdns.org'; // for WebSocket connections
-// const API_BASE_URL = 'http://localhost:3000';
-const API_BASE_URL = 'https://restaurant-server.duckdns.org';
+// const WS_BASE_URL = 'wss://restaurant-server.duckdns.org'; // for WebSocket connections
+const API_BASE_URL = 'http://192.168.8.148:3000';
+// const API_BASE_URL = 'https://restaurant-server.duckdns.org';
 
 export { API_BASE_URL, WS_BASE_URL };
--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
+export default {
   server: {
-    port: 5001, // your desired port number
-  },
-})
+    host: '0.0.0.0',
+    port: 5001, // optional
+  }
+}
